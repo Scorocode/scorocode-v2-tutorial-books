719f6ec879591814375fb8b165e7745a17effba3
gromver <gromver5@gmail.com>
2019-04-11 11:04:50 +0300

    feat(deps): install @scorocode/client-sdk feat(deps): install react-router-dom feat(deps): install styled-components feat(deps): install antd feat(deps): install prop-types chore: add .env hack to obtain absolute imports

--- src/App.js
+++ src/App.js
@@ -1,28 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Alert } from 'antd';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import styled, { createGlobalStyle } from 'styled-components';
+import 'antd/dist/antd.css';
+
+const GlobalStyle = createGlobalStyle`
+  html, body, #root {
+    height: 100%;
+    width: 100%;
+  }
+`;
+
+const Wrapper = styled.div`
+  height: 100%;
+  padding: 15px;
+`;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Wrapper>
+        <GlobalStyle />
+
+        <Router>
+          <div>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+            </ul>
+
+            <hr />
+
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+          </div>
+        </Router>
+      </Wrapper>
     );
   }
+}
+
+function Home() {
+  return <Alert message="Home page" />
+}
+
+function About() {
+  return <Alert type="warning" message="About page" />
 }
 
 export default App;
---
+++ src/sc.js
@@ -0,0 +1,7 @@
+import sc from '@scorocode/client-sdk';
+
+sc.initApp({
+  appId: 'c09784de48f44e15a3982d46d4074c6a',
+});
+
+export default sc;
